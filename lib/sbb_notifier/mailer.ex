ac5e8b41ffa791e58057e6ee7de98c6d6ed0f099
Kriegslustig <kriegslustig@kriegslustig.me>
2016-04-09 14:13:50 +0200

    Add parameters for the subject and text to Mailer

--- lib/sbb_notifier/mailer.ex
+++ lib/sbb_notifier/mailer.ex
@@ -3,11 +3,11 @@ defmodule SbbNotifier.Mailer do
           key: Application.get_env(:sbb_notifier, :mailgun_key)
   use Mailgun.Client, @config
 
-  def notify(address) do
+  def notify(address, s, t) do
     send_email to: address,
                from: "rando@kriegslustig.me",
-               subject: "SBB: ...",
-               text: "YOLO!"
+               subject: "SBB Notifier: " ++ s,
+               text: t
   end
 end
 
